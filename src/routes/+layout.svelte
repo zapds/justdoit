66b540b1e8e7b1062cbf8c251afc350b7a906ab0
zapds <71247896+zapds@users.noreply.github.com>
2025-06-19 13:09:04 +0530

    add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,28 @@
 <script>
 	import '../app.css';
+	import * as Avatar from '$lib/components/ui/avatar';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { Check, MenuIcon } from 'lucide-svelte';
 	
-	let { children } = $props();
+	let { children, data } = $props();
 </script>
+
+<div class="h-16 mx-auto flex max-w-7xl flex-row gap-4 items-center">
+	<div>
+		<a href="/">
+			<Check class="h-8 w-8 text-primary" />
+		</a>
+	</div>
+	<div class="ml-auto">
+		{#if data?.user}
+			<Avatar.Root class="ml-auto">
+				<Avatar.Image src={data.user.picture} alt="User Avatar" />
+				<Avatar.Fallback>{data.user.name[0]}</Avatar.Fallback>
+			</Avatar.Root>
+		{:else}
+			<MenuIcon class="ml-auto" />
+		{/if}
+	</div>
+</div>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
     import Button from '$lib/components/ui/button/button.svelte';
     import Image from "$lib/google-icon-logo-svgrepo-com.svg";
+    import { Check } from 'lucide-svelte';
     
     let { data } = $props();
 </script>
 
-<div class="h-screen flex flex-col items-center justify-center gap-8">
-
+<div class="h-[calc(100vh-64px)] flex flex-col items-center pt-32 gap-8">
+    
+    <Check class="h-48 w-48 text-primary" />
     <h1 class="text-8xl font-extrabold tracking-tighter">Just Do It.</h1>
     <Button size="lg" data-sveltekit- href={data?.user ? "/home" : "/login/google"} class="inline-flex">
         {#if data?.user}
